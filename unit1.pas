4107847eefe8d10f4c914b68b31fa4dd1812fc6c
Nico Straube <74559993+NicoStraube@users.noreply.github.com>
2021-01-19 13:43:29 +0100

    final

--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,29 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  ActnList;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    buttonCalc: TButton;
+    checkBox: TCheckBox;
+    comboBox: TComboBox;
+    editPrice: TLabeledEdit;
+    editPersons: TLabeledEdit;
+    editDays: TLabeledEdit;
+    editECharge: TLabeledEdit;
+    editPriceN: TLabeledEdit;
+    labelError: TLabel;
+    editDiscount: TLabeledEdit;
+    editPriceE: TLabeledEdit;
+    procedure buttonCalcClick(Sender: TObject);
+    procedure checkBoxChange(Sender: TObject);
+    procedure comboBoxChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -22,5 +41,135 @@ implementation
 
 {$R *.lfm}
 
-end.
+{ TForm1 }
 
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.comboBoxChange(Sender: TObject);
+begin
+  case comboBox.ItemIndex of
+    0: editPrice.Text := '123.-€';
+    1: editPrice.Text := '99.-€';
+    2: editPrice.Text := '186.-€';
+    3: editPrice.Text := '152.-€';
+    4: editPrice.Text := '134.-€';
+    5: editPrice.Text := '132.-€';
+  end;
+
+  checkBox.Enabled := True;
+  editPersons.Enabled := True;
+  editDays.Enabled := True;
+  buttonCalc.Enabled := True;
+
+
+  checkBox.Checked := False;
+  editECharge.Text := '-';
+  editPersons.Text := '';
+  editDays.Text := '';
+end;
+
+procedure TForm1.checkBoxChange(Sender: TObject);
+begin
+
+  case checkBox.Checked of
+    True:
+    begin
+      case comboBox.ItemIndex of
+        0: editECharge.Text := '15.-€';
+        1: editECharge.Text := '12.-€';
+        2: editECharge.Text := '15.-€';
+        3: editECharge.Text := '15.-€';
+        4: editECharge.Text := '15.-€';
+        5: editECharge.Text := '15.-€';
+      end;
+    end;
+    False: editECharge.Text := '-';
+  end;
+
+end;
+
+procedure TForm1.buttonCalcClick(Sender: TObject);
+var
+  price, eCharge, priceN, discount: currency;
+  persons, days: integer;
+  discountBool: boolean;
+begin
+  price := StrToCurr(StringReplace(editPrice.Text, '.-€', '', [rfReplaceAll]));
+  persons := 0;
+  days := 0;
+
+  if (checkBox.Checked) then
+    eCharge := StrToCurr(StringReplace(editECharge.Text, '.-€', '', [rfReplaceAll]));
+
+  try
+    persons := StrToInt(editPersons.Text);
+  except
+    labelError.Caption := 'Invalid datatype at "Personen". | ' + TimeToStr(Time());
+    editPersons.Clear;
+    editPriceN.Text := '-';
+    editDiscount.Text := '-';
+    editPriceE.Text := '-';
+  end;
+
+  try
+    days := StrToInt(editDays.Text);
+  except
+    labelError.Caption := 'Invalid datatype at "Tage". | ' + TimeToStr(Time());
+    editDays.Clear;
+    editPriceN.Text := '-';
+    editDiscount.Text := '-';
+    editPriceE.Text := '-';
+  end;
+
+
+  // calc Netto
+  if (checkBox.Checked) then
+  begin
+    priceN := (price + eCharge) * (persons * days);
+    editPriceN.Text := floatToStr(priceN) + '.-€';
+  end
+  else
+  begin
+    priceN := price * (persons * days);
+    editPriceN.Text := FloatToStr(priceN) + '.-€';
+  end;
+
+
+  // calc Rabatt
+  if (priceN >= 400) then
+  begin
+    discount := 3 * (priceN / 100);
+    discountBool := True;
+    editDiscount.Text := floatToStr(discount) + '.-€';
+  end
+  else if (priceN >= 600) then
+  begin
+    discount := 5 * (priceN / 100);
+    discountBool := True;
+    editDiscount.Text := floatToStr(discount) + '.-€';
+  end
+  else if (priceN >= 800) then
+  begin
+    discount := 7 * (priceN / 100);
+    discountBool := True;
+    editDiscount.Text := floatToStr(discount) + '.-€';
+  end
+  else if (priceN >= 1000) then
+  begin
+    discount := 9 * (priceN / 100);
+    discountBool := True;
+    editDiscount.Text := floatToStr(discount) + '.-€';
+  end;
+
+
+  // calc Endpreis
+  if (discountBool) then
+    editPriceE.Text := FloatToStr(priceN - discount) + '.-€'
+  else
+    editPriceE.Text := FloatToStr(priceN) + '.-€';
+end;
+
+end.
